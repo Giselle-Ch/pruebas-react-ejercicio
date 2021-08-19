15ba2b6a2465569fb14f49cfafc34e820195fe44
Giselle-Ch <giselle.chipagua@gmail.com>
2021-08-18 18:09:03 -0600

    primera version

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,20 +2,23 @@ import React,{useState} from 'react';
 import Todo from '../components/Todo';
 
 const Form = () => {
-    const [todo, setTodo] = useState({})
+    const [todo, setTodo] = useState({todo: '', cantidad: ''})
     const [todos, setTodos] = useState([
-        {todo: 'todo 1'},
-        {todo: 'todo 2'},
-        {todo: 'todo 3'}
+        {todo: 'todo 1', cantidad: '0'},
+        {todo: 'todo 2', cantidad: '3'},
+        {todo: 'todo 3', cantidad: '4'}
     ])
 
-    const handleChange = e => setTodo({[e.target.name]: e.target.value})
+
+    const handleChange = e => setTodo({ ...todo, [e.target.name]: e.target.value})
     const handleClick = e => {
-        if(Object.keys(todo).length === 0 || todo.todo.trim() === ''){
+        if(Object.keys(todo).length === 0 || todo.todo.trim() === '' || todo.cantidad.trim() === ''){
             alert('el campo no puede estar vacío')
             return
         }
+        
         setTodos([...todos, todo])
+        console.log(todo.todo + '-' + todo.cantidad)
     }
 
     const deleteTodo = indice => {
@@ -28,11 +31,12 @@ const Form = () => {
         <>
         <form onSubmit={e => e.preventDefault()}>
             <label>Agregar tarea</label><br />
-            <input type="text" name="todo" onChange={handleChange} />
+            <input type="text" name="todo" onChange={handleChange} placeholder="Elemento"/>
+            <input type="text" name="cantidad" onChange={handleChange} placeholder="Cantidad"/>
             <button onClick={handleClick}>agregar</button>
         </form>
         {
-            todos.map((value,index) => (<Todo todo={value.todo} key={index} index={index} deleteTodo={deleteTodo} />)) 
+            todos.map((value,index) => (<Todo todo={value.todo} cantidad={value.cantidad} key={index} index={index} deleteTodo={deleteTodo} />)) 
         }
         </>
     )
